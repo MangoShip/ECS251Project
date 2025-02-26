1f9cb844a5195a54a7dd0048dbb4bcea9410a6f7
zsuper <piyushkumbhare10@gmail.com>
2025-02-26 09:46:54 -0800

    Created a more extensive lib-test/stress-test.sh

--- lib-test/stress-test.sh
+++ lib-test/stress-test.sh
@@ -3,11 +3,19 @@ if [[ $# -ne 3 ]] then
     exit 1
 fi
 
+# This stress test simply runs /target/test with a desired number of threads, loops, and trials
+# In each trial, we get the output of the program. We should expect to see exactly NUM_LOOPS occurences of
+# the string "Tasks completed: [NUM_THREADS]". If we don't, then this script will exit and print the real
+# output of the program. For the printed output to be helpful, the library file tholder/lib/libtholder.a must
+# have print statements enabled
+
 for ((i = 0; i < $3; i++)); do
-    ret=$(target/test $1 $2 | grep -c $1)
-    if [[ $ret -ne $2 ]] then
+    ret=$(target/test $1 $2)
+    num_tasks=$(printf "%s\n" "$ret" | grep -c "Tasks completed: $1")
+    if [[ $num_tasks -ne $2 ]] then
         printf "\rTRIAL $i FAILED  \n"
-        echo $ret
+        echo Expected $2, got $num_tasks
+        printf "%s\n" "$ret"
         exit 2
     else
         printf "\rTRIAL $i SUCCESS"
